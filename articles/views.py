4443fe2c9388bb0e546dc81d1a3c8dd74171c1cb
Ivan <63307610+ShulaevIvan@users.noreply.github.com>
2022-05-22 15:31:06 +0300

    Update views.py

--- articles/views.py
+++ articles/views.py
@@ -10,9 +10,6 @@ def articles_list(request):
     context = {
         'object_list': object_list
     }
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     
 
     return render(request, template, context)
